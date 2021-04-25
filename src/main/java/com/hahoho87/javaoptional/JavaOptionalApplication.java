213fcac32f237174d578c82470aa9f43000f0c8c
hahoho87 <hahoho87@gmail.com>
2021-04-25 23:21:14 +0900

    Initial commit

---
+++ src/main/java/com/hahoho87/javaoptional/JavaOptionalApplication.java
@@ -0,0 +1,13 @@
+package com.hahoho87.javaoptional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOptionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaOptionalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hahoho87/javaoptional/JavaOptionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hahoho87.javaoptional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOptionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
